3e67a28dd5c0b65e018354b7546bd1e6b8c45f29
xentra-ehipolito <emmanuel.hipolito@xentragroup.com>
2026-06-26 23:50:18 +0800

    up

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -27,7 +27,7 @@
     <link href="{{ asset('vendors/perfect-scrollbar/perfect-scrollbar.css') }}" rel="stylesheet">
     <link href="{{ asset('vendors/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
-    <link rel="stylesheet" href="{{ asset('js/datatables/dataTables.bootstrap5.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('js/DataTables/dataTables.bootstrap5.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/base.css') }}">
      <!-- Select2 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
