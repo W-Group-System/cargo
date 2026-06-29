3685cea286d48417d901b255f54ea8b848a1802d
xentra-ehipolito <emmanuel.hipolito@xentragroup.com>
2026-06-29 15:57:59 +0800

    update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -114,7 +114,15 @@
     <!-- Select2 JS -->
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     @include('sweetalert::alert')
-    
+    <script>
+        @if(session('error'))
+            Swal.fire({
+                title: 'Unauthorized!',
+                text: @json(session('error')),
+                icon: 'error'
+            });
+        @endif
+    </script>
     @yield('footer')
 </body>
 </html>
