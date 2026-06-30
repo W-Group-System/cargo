e31290b18a39309d8b805691d25c54e09013513f
xentra-ehipolito <emmanuel.hipolito@xentragroup.com>
2026-06-30 09:35:31 +0800

    update

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class CronController extends Controller
 {
@@ -11,6 +13,7 @@ class CronController extends Controller
             "message"=>"This is a test API",
             "isSuccess"=>true
         ];
+        Log::info("TEST API RAN SUCCESSFULLY: ".Carbon::now());
         return response()->json($response,200);
     }
 }
