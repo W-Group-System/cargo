c3e65236fc90b02bbfc6ae92827e67d394ea5759
xentra-ehipolito <emmanuel.hipolito@xentragroup.com>
2026-07-06 10:00:51 +0800

    revert key up

--- resources/views/orders/indexV2.blade.php
+++ resources/views/orders/indexV2.blade.php
@@ -241,7 +241,7 @@ $(document).ready(function(){
         }
     });
 
-    $('#dateCreated,#buyersCode,#buyersName').on('change', function () {
+    $('#dateCreated,#buyersCode,#buyersName').on('change keyup', function () {
         ReloadDataTable();
     });
 
