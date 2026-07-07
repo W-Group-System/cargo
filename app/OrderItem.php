1268e7d0a2ba9edfd4a42502f529d0bae63cae29
xentra-ehipolito <emmanuel.hipolito@xentragroup.com>
2026-07-07 13:18:11 +0800

    Remove Audit Logs in Order Items

--- app/OrderItem.php
+++ app/OrderItem.php
@@ -1,13 +1,11 @@
 <?php
 
 namespace App;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\Model;
-use OwenIt\Auditing\Contracts\Auditable;
 
-class OrderItem extends Model implements Auditable
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
 {
-    use \OwenIt\Auditing\Auditable;
     
     protected $table = "order_items";
     protected $primaryKey = 'id';
